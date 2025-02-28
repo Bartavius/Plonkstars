a4efd1301e1cc7dda49ad50fa213333009d03f12
Patrick Zhang <pzhang345@gmail.com>
2025-02-28 02:23:39 -0500

    styled login

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -6,6 +6,7 @@ import { loginSuccess, loginFailure } from '../../redux/authSlice';
 import api from '../api'; 
 import { RootState } from '../../redux/store';
 import Cookies from 'js-cookie';
+import { motion } from 'framer-motion';
 
 const Login: React.FC = () => {
   const dispatch = useDispatch();
@@ -43,34 +44,48 @@ const Login: React.FC = () => {
   }
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            style={{color: 'black'}}
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    <div className="min-h-screen flex items-center justify-center">
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="w-full max-w-md p-6 bg-white shadow-lg rounded-2xl"
+      >
+        <div className="text-center mb-6">
+          {error && <div className="mb-4 text-red-500 text-center">{error}</div>}
+          <h2 className="text-xl font-semibold text-black">Login</h2>
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            style={{color: 'black'}}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+            <input
+              type="username"
+              id="username"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 text-center"
+          >
+            Sign In
+          </button>
+        </form>
+      </motion.div>
     </div>
   );
 };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,12 +4,17 @@ import React from 'react';
 import Login from './Login';
 import { Provider } from 'react-redux';
 import {store} from '../../redux/store';
+import NavBar from '@/components/Navbar';
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Provider store={store}>
-      <Login />
-      </Provider>
+    <div className="flex flex-col min-h-screen">
+      <div className="fixed top-0 left-0 w-full z-20">
+        <NavBar />
+          <Provider store={store}>
+            <Login />
+          </Provider>
+      </div>
     </div>
   );
 };
