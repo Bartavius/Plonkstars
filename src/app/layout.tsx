95425745e9fe7e4778c9f592c7b122f7a7f2494e
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-03-03 17:16:15 -0500

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "leaflet/dist/leaflet.css";
 import "./globals.css";
 import ReduxProvider from "./reduxProvider";
@@ -37,6 +38,7 @@ export default function RootLayout({
           <ReinstateSession />
           {children}
         </ReduxProvider>
+        <Analytics />
       </body>
     </html>
   );
