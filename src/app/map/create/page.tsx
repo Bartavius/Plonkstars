2bad8a1da9bbe7477aa773b287ff16c7dac39b19
Patrick Zhang <pzhang345@gmail.com>
2025-03-07 03:28:47 -0500

    fixed window problem

--- src/app/map/create/page.tsx
+++ src/app/map/create/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import BasicMapResult from "@/components/maps/BasicMapResult";
 import api from "@/utils/api";
+import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
+
+const BasicMapResult = dynamic(() => import("@/components/maps/BasicMapResult"), { ssr: false });
 
 const MAPID = "c5ce2041-a992-44f1-a8e6-104c6d6d4a43"
 export default function Results() {
