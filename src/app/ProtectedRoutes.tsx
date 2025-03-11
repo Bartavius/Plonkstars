6b6c958bf54fee6033e3642fb5bb5d53dacd79f9
Patrick Zhang <pzhang345@gmail.com>
2025-03-11 18:23:28 -0400

    fixed protected routes

--- src/app/ProtectedRoutes.tsx
+++ src/app/ProtectedRoutes.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 
 export default function ProtectedRoutes({children}: Readonly<{children: React.ReactNode}>) {
     const { isAuthenticated } = useSelector((state: any) => state.auth);
-    if (!isAuthenticated) {
+    if (isAuthenticated === false) {
         redirect("/account/login");
     }
     return <>{children}</>;
