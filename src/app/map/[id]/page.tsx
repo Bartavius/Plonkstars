fdb01fbd302655f519b8586485c7227ca646e5c6
Patrick Zhang <pzhang345@gmail.com>
2025-03-17 00:30:19 -0400

    maybe fixed error

--- src/app/map/[id]/page.tsx
+++ src/app/map/[id]/page.tsx
@@ -15,7 +15,7 @@ import api from "@/utils/api";
 import "./page.css"
 import { useDispatch } from "react-redux";
 import MapPreview from "@/components/maps/MapPreview";
-import StatBox from "./StatBox";
+import StatBox from "./atBox";
 
 interface Location {
     lat:number,
@@ -168,7 +168,7 @@ export default function MapInfoPage(){
     return (
         <div className="relative">
             <div className="navbar-buffer"/>
-            <IoMdArrowRoundBack className="map-search-back mouse-pointer" onClick={()=>{redirect("/map")}}/>
+            <IoMdArrowRoundBack className="map-search-back mouse-pointer" onClick={()=>{router.push("/map")}}/>
             <motion.div
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
