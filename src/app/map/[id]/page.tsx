77b7fcbc7d2550b70908149f87ec99d05b67a261
Patrick Zhang <pzhang345@gmail.com>
2025-03-17 00:47:04 -0400

    added back back arrow

--- src/app/map/[id]/page.tsx
+++ src/app/map/[id]/page.tsx
@@ -81,6 +81,10 @@ export default function MapInfoPage(){
         setLoading(false);
     } 
 
+    const goBack = () => {
+        router.push("/map");
+    }
+
     const distanceString = (distance:number) => {
         if(distance < 0) return {stat: "N/A"};
         const m = Math.round(distance * 1000);
@@ -169,7 +173,7 @@ export default function MapInfoPage(){
     return (
         <div className="relative">
             <div className="navbar-buffer"/>
-            {/* <IoMdArrowRoundBack className="map-search-back mouse-pointer" onClick={()=>{router.push("/map")}}/> */}
+            <IoMdArrowRoundBack className="map-search-back mouse-pointer" onClick={goBack}/>
             <motion.div
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
