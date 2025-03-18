fe758f3321e4dd9cbcee323f6b3259ef7df1ba05
Patrick Zhang <pzhang345@gmail.com>
2025-03-18 14:27:44 -0400

    starting settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,11 @@
+import "./page.css"
+export default function Settings() {
+    return (
+        <div>
+            <div className="navbar-buffer"/>
+            <div className="settings-wrapper">
+                <div className="settings-header">Settings</div>
+            </div>
+        </div>
+    )
+}
