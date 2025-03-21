67114340f9dccdb906abc2ef9e1f43f42f8e6d26
Patrick Zhang <pzhang345@gmail.com>
2025-03-20 23:31:47 -0400

    hopefully buttons on vercel work

--- next.config.ts
+++ next.config.ts
@@ -5,4 +5,17 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
 };
 
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cache-Control", value: "no-store, max-age=0" },
+        ],
+      },
+    ];
+  },
+};
+
 export default nextConfig;
