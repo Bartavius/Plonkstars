049a14033f3ca34878baa73b0b20467558cd329d
Patrick Zhang <pzhang345@gmail.com>
2025-03-22 22:38:17 -0400

    styled leaderboard page a little

--- src/app/map/[id]/leaderboard/page.tsx
+++ src/app/map/[id]/leaderboard/page.tsx
@@ -2,6 +2,7 @@
 
 import MapLeaderboard from "@/components/maps/leaderboard/leaderboard";
 import { useParams, useRouter } from "next/navigation";
+import "./page.css";
 
 export default function MapLeaderboardPage(){
     const MAPID = useParams().id;
@@ -15,7 +16,14 @@ export default function MapLeaderboardPage(){
     return (
         <div>
             <div className="navbar-buffer"/>
-            <MapLeaderboard mapID={MAPID.toString()}/>
+            <div className="title-header-wrapper">
+                <div className="title-header">Leaderboard</div>
+            </div>
+            <div className="map-leaderboard-wrapper">
+                <div className="map-leaderboard-box">
+                    <MapLeaderboard mapID={MAPID.toString()}/>
+                </div>
+            </div>
         </div>
     )
 }
