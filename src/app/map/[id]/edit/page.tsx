ece96774b9a337ac08e145071cf5591c72057f7b
Patrick Zhang <pzhang345@gmail.com>
2025-04-04 16:35:14 -0400

    after removing a point the grayed out button now says add bound

--- src/app/map/[id]/edit/page.tsx
+++ src/app/map/[id]/edit/page.tsx
@@ -156,6 +156,7 @@ export default function EditMapPage() {
                 setBounds([...bounds,res.data]);
             }
             setSelectedBound(undefined);
+            setExistingBound(false);
         } catch (error) {}
     }
 
