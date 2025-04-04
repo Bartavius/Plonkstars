ccbe972c21bf4030b3e1a3f9296430c9e5ce61b8
Patrick Zhang <pzhang345@gmail.com>
2025-04-04 13:51:27 -0400

    bounds are now selectable in map editing tool

--- src/components/maps/MapPreview.tsx
+++ src/components/maps/MapPreview.tsx
@@ -10,6 +10,7 @@ import "leaflet/dist/leaflet.css";
 import MapIcon from "./mapIcon";
 import "./map.css";
 import getTileLayer from "../../utils/leaflet";
+import { useState } from "react";
 
 interface Location {
     lat:number,
@@ -26,13 +27,16 @@ const MapPreview = ({
     children,
     iconClick,
     height,
+    onSelect
 }: {
     bounds: (Bounds|Location)[];
     children?: React.ReactNode;
     onClick?: (event:LeafletMouseEvent) => void;
     iconClick?: boolean;
     height?: number;
+    onSelect?: (location: Location|Bounds) => void;
 }) => {
+    const [selected,setSelected] = useState<Bounds|Location>();
     const points = bounds.filter((marker) => {
         return "lat" in marker && "lng" in marker;
     });
@@ -45,7 +49,12 @@ const MapPreview = ({
     const PX_PER_ZOOM_LEVEL = 2;
     const CENTER = { lat: 0, lng: 0 };
     const maxBounds = L.latLngBounds({lat:-85, lng:-240},{lat:85, lng:240})
-        
+    
+    const select = (location: Location|Bounds|undefined) => {
+        setSelected(location);
+    }
+
+
     return (
         <div
             className="leaflet-container-result-wrapper map-result-container"
@@ -65,20 +74,41 @@ const MapPreview = ({
                 {children}
                 <div>
                     {points.map((point,index) => (
-                        <MapIcon key={index} pos={point} clickable={iconClick} iconUrl="/PlonkStarsMarker.png"/>
+                        <div key={index}>
+                        {point !== selected && 
+                            <MapIcon pos={point} clickable={iconClick || onSelect !== undefined} iconUrl="/PlonkStarsMarker.png" onClick={onSelect ? () => select(point) : undefined}/>
+                        }
+                        {point == selected && 
+                            <MapIcon pos={point} clickable={iconClick || onSelect !== undefined} iconUrl="/PlonkStarsAvatar.png" onClick={onSelect ? () => select(undefined) : undefined}/>
+                        }
+                        </div>
                     ))}
                 </div>
                 <div>
                     {area.map((bound,index) => (
+                        <div key={index}>
+                        {bound !== selected && 
                             <Rectangle
-                                key={index}
+                                interactive = {onSelect !== undefined}
+                                eventHandlers={onSelect ? { click: () => select(bound) } : undefined}
                                 bounds={[[bound.start.lat,bound.start.lng],[bound.end.lat,bound.end.lng]]}
                                 color="yellow"
                                 opacity={1}
                                 weight={0}
                             />
-                        ))
-                    }
+                        }
+                        {bound === selected && 
+                            <Rectangle
+                                interactive = {onSelect !== undefined}
+                                eventHandlers={onSelect ? { click: () => select(undefined) } : undefined}
+                                bounds={[[bound.start.lat,bound.start.lng],[bound.end.lat,bound.end.lng]]}
+                                color="blue"
+                                opacity={1}
+                                weight={0}
+                            />
+                        }
+                        </div>
+                    ))}
                 </div>
             </MapContainer>
         </div>
