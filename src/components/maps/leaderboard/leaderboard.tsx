e5d732e3dad39bbba4c424f0cd8dd70b328aca9b
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-04-05 22:48:11 -0400

    naming error in leaderboard

--- src/components/maps/leaderboard/leaderboard.tsx
+++ src/components/maps/leaderboard/leaderboard.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import Table from "@/components/maps/leaderboard/table";
+import Table from "@/components/maps/table/table";
 import Loading from "@/components/loading";
 import api from "@/utils/api";
 import { FaArrowRight } from "react-icons/fa";
