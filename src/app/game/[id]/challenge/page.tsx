771998044fb2ebdd84d88a8aa2caac862464a563
Patrick Zhang <pzhang345@gmail.com>
2025-04-11 14:18:42 -0400

    added challenge endpoint

---
+++ src/app/game/[id]/challenge/page.tsx
@@ -0,0 +1,10 @@
+import "./page.css";
+export default function challengePage() {
+    const hostname = "rgirjirjgrjgj";
+    const mapname = "mapname";
+    return (
+        <div className="centered-elements">
+            <div>You got challenged by {hostname} on mapname</div>
+        </div>
+    )
+}
