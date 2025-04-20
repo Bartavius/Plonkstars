ccc29c9bc3b1d9d059dc67e2135e76aef44f2837
Patrick Zhang <pzhang345@gmail.com>
2025-04-19 23:50:46 -0400

    deleted unnecessary variables from customize page

--- src/app/customize/page.tsx
+++ src/app/customize/page.tsx
@@ -4,36 +4,11 @@ import maps from "@/utils/maps";
 import "./page.css";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setMapType } from "@/redux/settingsSlice";
 import Popup from "@/components/Popup";
-import api from "@/utils/api";
 import AvatarCustom from "./avatar";
-
-interface UserIcon {
-  hue: number;
-  saturation: number;
-  brightness: number;
-}
-
-const MAX_HUE = 360;
-const MAX_SATURATION = 300;
-const MAX_BRIGHTNESS = 200;
-const colorPresets = [
-  { name: "Red", color: "#FF3B4A", hue: 315, saturation: 300, brightness: 100 },
-  { name: "Orange", color: "#FF8300", hue: 331, saturation: 255, brightness: 144 },
-  { name: "Yellow", color: "#FFFF00", hue: 0, saturation: 300, brightness: 200 },
-  { name: "Green", color: "#00f734", hue: 81, saturation: 228, brightness: 116 },
-  { name: "Blue", color: "#00d8ff", hue: 183, saturation: 236, brightness: 137 },
-  { name: "Bluer", color: "#298fff", hue: 192, saturation: 273, brightness: 97 },
-  { name: "Pink", color: "#ff95ff", hue: 254, saturation: 158, brightness: 142 },
-  { name: "Lilac", color: "#C77CE0", hue: 240, saturation: 100, brightness: 100 },
-  { name: "Purple", color: "#9274ff", hue: 210, saturation: 300, brightness: 97 },
-  { name: "Black", color: "#000000", hue: 0, saturation: 0, brightness: 0 },
-  { name: "Grey", color: "#A1A1A1", hue: 0, saturation: 0, brightness: 110 },
-  { name: "White", color: "#FFFFFF", hue: 0, saturation: 0, brightness: 200 },
-];
 
 export default function Settings() {
   const mapNumber = useSelector((state: any) => state.settings.mapNumber);
