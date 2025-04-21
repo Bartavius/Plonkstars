44f08292d90ca1322f6ca0375a1515ddb57a8bb9
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-04-20 23:34:26 -0400

    avatar customization sliders adjust dynamically to hue changes

--- src/app/customize/avatar.tsx
+++ src/app/customize/avatar.tsx
@@ -13,7 +13,11 @@ const MAX_HUE = 360;
 const MAX_SATURATION = 300;
 const MAX_BRIGHTNESS = 200;
 
-export default function AvatarCustom({setMessage} : {setMessage: (message: string) => void}) {
+export default function AvatarCustom({
+  setMessage,
+}: {
+  setMessage: (message: string) => void;
+}) {
   const [userIconDefault, setUserIconDefault] = useState<UserIcon>({
     hue: 0,
     saturation: 150,
@@ -82,108 +86,132 @@ export default function AvatarCustom({setMessage} : {setMessage: (message: strin
 
   return (
     <div className="settings-box">
-          <div className="settings-label">Avatar Customization</div>
-          <div className="avatar-customizer-container">
-            <div className="avatar-preview">
-              <img
-                src="/PlonkStarsAvatar.png"
-                alt="avatar-icon-preview"
-                className="avatar-icon"
-                style={{ filter: getFilterStyle() }}
+      <div className="settings-label">Avatar Customization</div>
+      <div className="avatar-customizer-container">
+        <div className="avatar-preview">
+          <img
+            src="/PlonkStarsAvatar.png"
+            alt="avatar-icon-preview"
+            className="avatar-icon"
+            style={{ filter: getFilterStyle() }}
+          />
+        </div>
+
+        {/* Right side: Color adjustment controls */}
+        <div className="color-controls">
+          <div className="text-center">
+            <span className="">Set Colors</span>
+            <div className="text-center justify-center flex">
+              <div className="color-presets">
+                {colorPresets.map((preset) => (
+                  <div
+                    className="flex-column align-center justify-center"
+                    key={preset.name}
+                  >
+                    <label htmlFor={preset.name}>{preset.name}</label>
+                    <div
+                      id={preset.name}
+                      key={preset.name}
+                      className="color-preset justify-center flex"
+                      style={{ backgroundColor: preset.color }}
+                      onClick={() => handlePresetClick(preset)}
+                      title={preset.name}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* Hue adjustment */}
+          <div className="control-group">
+            <label>Hue</label>
+            <div className="slider-wrapper">
+              <div
+                className="hue-slider-background"
+                style={{
+                  filter: `saturate(${userIcon.saturation}%) 
+                brightness(${userIcon.brightness}%)`,
+                }}
+              />
+              <input
+                type="range"
+                min="0"
+                max={`${MAX_HUE}`}
+                value={userIcon.hue}
+                className="slider hue-slider"
+                onChange={(e) =>
+                  handleSliderChange("hue", parseInt(e.target.value))
+                }
               />
             </div>
-
-            {/* Right side: Color adjustment controls */}
-            <div className="color-controls">
-              <div className="text-center">
-                <span className="">Set Colors</span>
-                <div className="text-center justify-center flex">
-                  <div className="color-presets">
-                    {colorPresets.map((preset) => (
-                      <div className="flex-column align-center justify-center" key={preset.name}>
-                        <label htmlFor={preset.name}>{preset.name}</label>
-                        <div
-                          id={preset.name}
-                          key={preset.name}
-                          className="color-preset justify-center flex"
-                          style={{ backgroundColor: preset.color }}
-                          onClick={() => handlePresetClick(preset)}
-                          title={preset.name}
-                        />
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              {/* Hue adjustment */}
-              <div className="control-group">
-                <label>Hue</label>
-                <input
-                  type="range"
-                  min="0"
-                  max={`${MAX_HUE}`}
-                  value={userIcon.hue}
-                  className="slider hue-slider"
-                  onChange={(e) =>
-                    handleSliderChange("hue", parseInt(e.target.value))
-                  }
-                />
-                <span className="value-display">{userIcon.hue}°</span>
-              </div>
-
-              {/* Saturation adjustment */}
-              <div className="control-group">
-                <label>Saturation</label>
-                <input
-                  type="range"
-                  min="0"
-                  max={`${MAX_SATURATION}`}
-                  value={userIcon.saturation}
-                  className="slider saturation-slider"
-                  onChange={(e) =>
-                    handleSliderChange("saturation", parseInt(e.target.value))
-                  }
-                />
-                <span className="value-display">{userIcon.saturation}%</span>
-              </div>
-
-              {/* Brightness adjustment */}
-              <div className="control-group">
-                <label>Brightness</label>
-                <input
-                  type="range"
-                  min="0"
-                  max={`${MAX_BRIGHTNESS}`}
-                  value={userIcon.brightness}
-                  className="slider brightness-slider"
-                  onChange={(e) =>
-                    handleSliderChange("brightness", parseInt(e.target.value))
-                  }
-                />
-                <span className="value-display">{userIcon.brightness}%</span>
-              </div>
-            </div>
+            <span className="value-display">{userIcon.hue}°</span>
           </div>
-          <div>
-            <button
-              className="form-button-general mr-3"
-              onClick={() =>
-                setUserIcon({
-                  hue: userIconDefault.hue,
-                  saturation: userIconDefault.saturation,
-                  brightness: userIconDefault.brightness,
-                })
-              }
-            >
-              Reset
-            </button>
-            <button
-              className="form-button-selected"
-              onClick={() => saveAvatarChanges()}
-            >
-              Save Changes
-            </button>
+
+          {/* Saturation adjustment */}
+          <div className="control-group">
+            <label>Saturation</label>
+            <div className="slider-wrapper">
+              <div
+                className="saturation-slider-background"
+                style={{ filter: `hue-rotate(${userIcon.hue}deg) `}}
+              />
+              <input
+                type="range"
+                min="0"
+                max={`${MAX_SATURATION}`}
+                value={userIcon.saturation}
+                className="slider saturation-slider"
+                onChange={(e) =>
+                  handleSliderChange("saturation", parseInt(e.target.value))
+                }
+              />
+            </div>
+            <span className="value-display">{userIcon.saturation}%</span>
+          </div>
+
+          {/* Brightness adjustment */}
+          <div className="control-group">
+            <label>Brightness</label>
+            <div className="slider-wrapper">
+              <div
+                className="brightness-slider-background"
+                style={{ filter: `hue-rotate(${userIcon.hue}deg) saturate(${userIcon.saturation}%)` }}
+              />
+              <input
+                type="range"
+                min="0"
+                max={`${MAX_BRIGHTNESS}`}
+                value={userIcon.brightness}
+                className="slider brightness-slider"
+                onChange={(e) =>
+                  handleSliderChange("brightness", parseInt(e.target.value))
+                }
+              />
+            </div>
+            <span className="value-display">{userIcon.brightness}%</span>
           </div>
         </div>
-  )
+      </div>
+      <div>
+        <button
+          className="form-button-general mr-3"
+          onClick={() =>
+            setUserIcon({
+              hue: userIconDefault.hue,
+              saturation: userIconDefault.saturation,
+              brightness: userIconDefault.brightness,
+            })
+          }
+        >
+          Reset
+        </button>
+        <button
+          className="form-button-selected"
+          onClick={() => saveAvatarChanges()}
+        >
+          Save Changes
+        </button>
+      </div>
+    </div>
+  );
 }
