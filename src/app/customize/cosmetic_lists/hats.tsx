f03394e46b67e3d324b43b6c631156939c127cf1
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-05-15 02:41:09 +0700

    removed some more

--- src/app/customize/cosmetic_lists/hats.tsx
+++
@@ -1,8 +0,0 @@
-// import Fedora from "@/components/cosmetics/hats/fedora";
-// import NoHat from "@/components/cosmetics/hats/NoHat";
-// import { CosmeticTiers } from "@/types/CosmeticTiers";
-
-// export const hats = [
-//     { name: "no_hat", tier: CosmeticTiers.COMMON, html: <NoHat /> },
-//     { name: "fedora", tier: CosmeticTiers.EPIC, html: <Fedora /> },
-//   ];
