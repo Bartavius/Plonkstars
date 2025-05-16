81078be7c78e456e3bde40fa5f8821cd12e2e8c3
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-05-16 23:03:53 +0700

    revamped lootbox functionality

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -6,17 +6,11 @@ import { CosmeticTiers } from "@/types/CosmeticTiers";
 import api from "@/utils/api";
 import { useEffect, useState } from "react";
 
-interface CrateProps {
-  name: string;
-  price: number;
-  rarity: CosmeticTiers;
-}
-
 export default function Page() {
   const [message, _setMessage] = useState<React.ReactNode>();
   const [update, setUpdate] = useState<number>(0);
   const [type, setType] = useState("");
-  const [cosmetic, setCosmetic] = useState<CosmeticProps | null>(null);
+  const [response, setResponse] = useState<Response | null>(null);
 
   const setMessage = (message: React.ReactNode) => {
     _setMessage(message);
@@ -30,38 +24,13 @@ export default function Page() {
       setCoins(parseInt(data.coins));
     };
     fetchCoins();
-  }, []);
+  }, [response]);
 
-  const crates = [
-    { name: "Common Crate", price: 100, rarity: CosmeticTiers.COMMON },
-    { name: "Uncommon Crate", price: 250, rarity: CosmeticTiers.UNCOMMON },
-    { name: "Rare Crate", price: 500, rarity: CosmeticTiers.RARE },
-    { name: "Epic Crate", price: 1000, rarity: CosmeticTiers.EPIC },
-    { name: "Legendary Crate", price: 2000, rarity: CosmeticTiers.LEGENDARY },
-  ];
-
-  // Map rarities to Tailwind color classes
-  const rarityColors: Record<CosmeticTiers, string> = {
-    COMMON: "bg-gray-300 text-gray-800",
-    UNCOMMON: "bg-green-300 text-green-900",
-    RARE: "bg-blue-400 text-blue-900",
-    EPIC: "bg-purple-400 text-purple-900",
-    LEGENDARY: "bg-yellow-400 text-yellow-900 font-bold",
-  };
-
-  const darkerRarityColors: Record<CosmeticTiers, string> = {
-    COMMON: "bg-gray-500 text-gray-800",
-    UNCOMMON: "bg-green-600 text-green-900",
-    RARE: "bg-blue-600 text-blue-900",
-    EPIC: "bg-purple-600 text-purple-900",
-    LEGENDARY: "bg-yellow-600 text-yellow-900 font-bold",
-  };
-
-  const buyCrate = async (crate: CrateProps) => {
+  const buyCrate = async (crate: Crate) => {
     try {
       const response = await api.post("/account/profile/buy", crate);
       setMessage(response.data.message || "Purchase successful!");
-      setCosmetic(response.data.cosmetic);
+      setResponse(response.data);
       setType("success");
     } catch (err: any) {
       const errorMsg =
@@ -77,32 +46,43 @@ export default function Page() {
       <div className="navbar-buffer" />
       <Popup update={update} type={type}>
         {message}
-        {cosmetic && type == "success" && (
+
+    {/* //     "cosmetic": reward.to_json() if hasattr(reward, 'to_json') else None,
+    //     "coins": reward if isinstance(reward, int) else None,
+    //     "duplicate": duplicate,
+    //     "tier": result_tier,
+    //     "message": f"{crate_name.capitalize()} Crate purchased successfully!"
+    //  */}
+
+
+        {response && type == "success" && (
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
             <div
               className={`w-[50%] h-[60%] p-6 ${
-                rarityColors[cosmetic.tier]
+                rarityColors[response.tier]
               } rounded-xl shadow-2xl text-center animate-reward-pop relative`}
             >
               <h2 className="text-2xl font-bold mb-4">
-                {cosmetic.tier} Cosmetic Unlocked!
+                {response.duplicate && `You've unlocked a duplicate ${response.tier} cosmetic. Instead, you'll be awarded with...` }
+                {response.cosmetic && `${response.cosmetic.tier} Cosmetic Unlocked!`}
+                {response.tier === 'COINS' && `You've won...`}
               </h2>
               <div className="w-full h-64 flex justify-center items-center">
+                {response.cosmetic ?
                 <img
-                  src={`/cosmetics/${cosmetic.type}/${cosmetic.image}`}
-                  alt={cosmetic.image.toString()}
+                  src={`/cosmetics/${response.cosmetic.type}/${response.cosmetic.image}`}
+                  alt={response.cosmetic.image.toString()}
                   className="max-h-full max-w-full object-contain"
-                />
+                /> :
+                <img src="/cosmetics/coin.png" alt="coin" className="max-h-full max-w-full object-contain"/>
                 
                 
+}
               </div>
               <p className="text-4xl font-bold text-black mt-5">
-                {cosmetic.item_name}
+                {response.cosmetic ? response.cosmetic.item_name : `${response.coins} coins`}
               </p>
-            
-              
             </div>
-            
           </div>
         )}
       </Popup>
@@ -115,12 +95,10 @@ export default function Page() {
         </div>
 
         <div className="flex flex-col gap-4">
-          {crates.map((crate: CrateProps) => (
+          {crates.map((crate: Crate) => (
             <div
               key={crate.name}
-              className={`flex justify-between items-center px-6 py-4 rounded-lg shadow cursor-pointer transform transition duration-150 hover:scale-105 hover:shadow-lg ${
-                rarityColors[crate.rarity]
-              }`}
+              className={`flex justify-between items-center px-6 py-4 rounded-lg shadow cursor-pointer transform transition duration-150 hover:scale-105 hover:shadow-lg`}
               onClick={() => buyCrate(crate)}
             >
               <div className="text-lg font-semibold">{crate.name}</div>
@@ -132,3 +110,107 @@ export default function Page() {
     </div>
   );
 }
+
+interface Response {
+    cosmetic: CosmeticProps | null;
+    coins: number | null;
+    duplicate: boolean;
+    tier: CosmeticTiers | 'COINS';
+    message: string;
+}
+
+// TODO: restructure so that each tier now has percentages on waht can spawn
+interface Crate {
+    name: string; // will need image name later
+    price: number;
+    percentages: Percentages;
+    coinsMin: number;
+    coinsMax: number;
+  }
+
+  interface Percentages {
+    COINS: number;
+    COMMON: number;
+    UNCOMMON: number;
+    RARE: number;
+    EPIC: number;
+    LEGENDARY: number;
+  }
+
+  const crates: Crate[] = [
+    {
+      name: "Wooden Crate",
+      price: 20,
+      percentages: {
+        COINS: 0.4,
+        COMMON: 0.58889,
+        UNCOMMON: 0.01,
+        RARE: 0.001,
+        EPIC: 0.0001,
+        LEGENDARY: 0.00001,
+      },
+      coinsMin: 0,
+      coinsMax: 0,
+    },
+    {
+      name: "Bronze Crate",
+      price: 500,
+      percentages: {
+        COINS: 0.1,
+        COMMON: 0.6,
+        UNCOMMON: 0.2,
+        RARE: 0.08,
+        EPIC: 0.015,
+        LEGENDARY: 0.005,
+      },
+      coinsMin: 100,
+      coinsMax: 500,
+    },
+    {
+      name: "Silver Crate",
+      price: 1000,
+      percentages: {
+        COINS: 0.15,
+        COMMON: 0.35,
+        UNCOMMON: 0.25,
+        RARE: 0.15,
+        EPIC: 0.07,
+        LEGENDARY: 0.03,
+      },
+      coinsMin: 250,
+      coinsMax: 800,
+    },
+    {
+      name: "Gold Crate",
+      price: 2000,
+      percentages: {
+        COINS: 0.2,
+        COMMON: 0.1,
+        UNCOMMON: 0.2,
+        RARE: 0.25,
+        EPIC: 0.15,
+        LEGENDARY: 0.1,
+      },
+      coinsMin: 500,
+      coinsMax: 1500,
+    },
+  ];
+
+
+  // Map rarities to Tailwind color classes
+  const rarityColors: Record<CosmeticTiers | 'COINS', string> = {
+    COINS: "bg-orange-300 text-orange-900",
+    COMMON: "bg-gray-300 text-gray-800",
+    UNCOMMON: "bg-green-300 text-green-900",
+    RARE: "bg-blue-400 text-blue-900",
+    EPIC: "bg-purple-400 text-purple-900",
+    LEGENDARY: "bg-yellow-400 text-yellow-900 font-bold",
+  };
+
+  const darkerRarityColors: Record<CosmeticTiers, string> = {
+    COMMON: "bg-gray-500 text-gray-800",
+    UNCOMMON: "bg-green-600 text-green-900",
+    RARE: "bg-blue-600 text-blue-900",
+    EPIC: "bg-purple-600 text-purple-900",
+    LEGENDARY: "bg-yellow-600 text-yellow-900 font-bold",
+  };
